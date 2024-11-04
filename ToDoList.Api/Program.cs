c76032f3f32537149ae1ee45be00d5d522a8a0e5
Anyan Zhao <AZhao4@slb.com>
2024-11-04 23:03:22 +0800

    feat: add exception filter

--- ToDoList.Api/Program.cs
+++ ToDoList.Api/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using TodoItem.Infrastructure;
+using TodoItems.Core;
 using ToDoList.Api.Dto;
 using ToDoList.Api.Services;
 
@@ -12,13 +15,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IToDoItemsService, ToDoItemService>();
 
 builder.Services.Configure<ToDoItemDatabaseSettings>(builder.Configuration.GetSection("ToDoItemDatabase"));
+builder.Services.AddSingleton<IToDoItemsService, ToDoItemService>();
+
+builder.Services.Configure<TodoStoreDatabaseSettings>(builder.Configuration.GetSection("ToDoItemDatabase"));
+
+builder.Services.AddSingleton<ITodosRepository, TodoItemMongoRepository>();
+builder.Services.AddSingleton<ITodoItemService, TodoItemService>();
+
 builder.Services.AddSingleton<IMongoClient>(sp =>
 {
     var settings = sp.GetRequiredService<IOptions<ToDoItemDatabaseSettings>>();
     return new MongoClient(settings.Value.ConnectionString);
+});
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<CustomExceptionFilter>();
 });
 
 var app = builder.Build();
@@ -39,3 +53,5 @@ app.MapControllers();
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.Run();
+
+public partial class Program { }
