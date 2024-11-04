6f437ade4e5a83d33e50e20395489a3f02e76731
Anyan Zhao <AZhao4@slb.com>
2024-11-04 18:55:44 +0800

    chores: add .gitignore

--- ToDoList.Api.Test/obj/Debug/net8.0/ToDoList.Api.Test.AssemblyInfo.cs
+++ ToDoList.Api.Test/obj/Debug/net8.0/ToDoList.Api.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList.Api.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227d7f036045c7c97cdbbfe66a8d219bcedfafb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList.Api.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList.Api.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
